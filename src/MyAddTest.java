2e1e0aeda1edc818a5e4e87c157f9da286424fd0
a <a@192.168.1.115>
2019-02-20 15:46:53 +0200

    HW2Testing8

--- src/MyAddTest.java
+++ src/MyAddTest.java
@@ -1,5 +1,3 @@
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class MyAddTest
@@ -7,8 +5,8 @@ public class MyAddTest
 	@Test
 	public void testAddcart() 
 	{
-		Cart ass = new Cart() ;
-		assertTrue(0 == ass.getTotal()) ;
+		Cart as = new Cart() ;
+		org.junit.Assert.assertTrue(0 == as.getTotal()) ;
 	}
 
 	@Test
@@ -17,8 +15,8 @@ public class MyAddTest
 		MyAdd r = new MyAdd() ;
 		r.addcart();
 		r.addProduct(0, 127,"Java Book", 1);
-		assertTrue(1==r.add1.get(0).prodList.size()) ;
-		assertTrue(127 == r.add1.get(0).prodList.get(0).getPrice()) ;
+		org.junit.Assert.assertTrue(1==r.add1.get(0).prodList.size()) ;
+		org.junit.Assert.assertTrue(127 == r.add1.get(0).prodList.get(0).getPrice()) ;
 		r.add1.get(0).prodList.clear();
 		r.add1.clear();
 	}
@@ -30,8 +28,8 @@ public class MyAddTest
 		y.addcart();
 		y.addProduct(0, 127,"Java Book", 1);
 		y.addProduct(0, 100,"Web desing Book", 1);
-		assertTrue(2==y.add1.get(0).prodList.size()) ;
-		assertTrue(227 == y.add1.get(0).getTotal()) ;	
+		org.junit.Assert.assertTrue(2==y.add1.get(0).prodList.size()) ;
+		org.junit.Assert.assertTrue(227 == y.add1.get(0).getTotal()) ;	
 		y.add1.get(0).prodList.clear();
 		y.add1.clear();
 	}
@@ -44,8 +42,8 @@ public class MyAddTest
 		z.addProduct(0, 127,"Java Book", 1);
 		z.addProduct(0, 100,"Web desing Book", 1);
 		z.addProduct(0, 120,"C++ Book", 1);
-		assertTrue(3==z.add1.get(0).prodList.size()) ;
-		assertTrue(347 == z.add1.get(0).getTotal()) ;	
+		org.junit.Assert.assertTrue(3==z.add1.get(0).prodList.size()) ;
+		org.junit.Assert.assertTrue(347 == z.add1.get(0).getTotal()) ;	
 		z.add1.get(0).prodList.clear();
 		z.add1.clear();
 	}
@@ -59,8 +57,8 @@ public class MyAddTest
 		b.addProduct(0, 100,"Web desing Book", 1);
 		b.addProduct(0, 120,"C++ Book", 1);
 		b.addProduct(0, 300,"Database Book", 1);
-		assertTrue(4==b.add1.get(0).prodList.size()) ;
-		assertTrue(647 == b.add1.get(0).getTotal()) ;	
+		org.junit.Assert.assertTrue(4==b.add1.get(0).prodList.size()) ;
+		org.junit.Assert.assertTrue(647 == b.add1.get(0).getTotal()) ;	
 		b.add1.get(0).prodList.clear();
 		b.add1.clear();
 	}
@@ -74,8 +72,8 @@ public class MyAddTest
 		v.addProduct(0, 100,"Web desing Book", 1);
 		v.addProduct(0, 120,"C++ Book", 1);
 		v.addProduct(0, 300,"Database Book", 1);
-		assertFalse(6==v.add1.get(0).prodList.size()) ;
-		assertFalse(600 == v.add1.get(0).getTotal()) ;	
+		org.junit.Assert.assertFalse(6==v.add1.get(0).prodList.size()) ;
+		org.junit.Assert.assertFalse(600 == v.add1.get(0).getTotal()) ;	
 		v.add1.get(0).prodList.clear();
 		v.add1.clear();
 	}
@@ -90,8 +88,8 @@ public class MyAddTest
 		k.addProduct(0, 120,"C++ Book", 1);
 		k.addProduct(0, 300,"Database Book", 1);
 		k.addProduct(0, 200,"DataStructure Book", 1);
-		assertFalse(7==k.add1.get(0).prodList.size()) ;
-		assertFalse(600 == k.add1.get(0).getTotal()) ;	
+		org.junit.Assert.assertFalse(7==k.add1.get(0).prodList.size()) ;
+		org.junit.Assert.assertFalse(600 == k.add1.get(0).getTotal()) ;	
 		k.add1.get(0).prodList.clear();
 		k.add1.clear();
 	}
